46aef33daa393f7e553d31e492c1f4444742a13a
pdthswown <80870908+pdthswown@users.noreply.github.com>
2021-05-20 12:49:20 +0900

    상품삭제 메소드 추가
    ItemMap.java / delItemList 메소드 추가
    계속추가 할수 있도록 반복기능 구현

--- ItemMap.java
+++ ItemMap.java
@@ -66,6 +66,58 @@ public class ItemMap {
 	//상품등록 메소드 종료
 	
 	
+	
+	
+	//상품삭제 메소드
+	public void delItemList() {
+		String findName;
+		String quest;
+		String conti;
+		
+		while(true) {		
+			System.out.println("삭제할 상품의 이름을 입력하세요\n");
+			findName = scan.next();
+		
+			try{
+				String[] infoList = itemList.get(findName);
+				System.out.println("이름 : "+findName);
+				for(int i=0; i<3; i++) {
+					System.out.println(infoList[i]);
+				}
+				System.out.println("위 상품의 정보를 정말 삭제하시겠습니까? (Y/N)");
+				quest = scan.next();
+				
+				if(quest.equals("Y") || quest.equals("y")) {
+					itemList.remove(findName);
+					System.out.println("삭제가 완료되었습니다.");
+				}else if(quest.equals("n") || quest.equals("N")) {
+					System.out.println("상품 삭제를 취소합니다.");
+				}else {
+					System.out.println("잘못입력하셨습니다.");
+				}
+				
+			}catch(NullPointerException e){
+				System.out.println("오류가 발생하였습니다.");
+				
+			}finally {
+			}
+			
+			System.out.println("상품삭제를 계속하시겠습니까? (Y/N)");
+			conti = scan.next();
+			
+			if(conti.equals("Y") || conti.equals("y")) {
+				System.out.println("상품 삭제를 계속합니다.");
+				
+			}else if(conti.equals("n") || conti.equals("N")) {
+				System.out.println("초기 메뉴로 돌아갑니다.");
+				break;
+			}
+		}
+	}
+	//상품삭제 메소드 종료
+	
+	
+	
 	//상품조회 메소드
 	public void findItem() {
 		String findName;
--- Main.java
+++ Main.java
@@ -6,27 +6,12 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		int choose;
+		ItemMap itemmap = new ItemMap();
 		
-		System.out.println("----- 물류 재고관리 시스템 -----");
-		System.out.println("       1. 입출고 관리");
-		System.out.println("       2. 재고 확인");
-		System.out.println("       3. 매출 확인");
-		System.out.println("----------------------------");
-		System.out.print("번호를 입력해 주세요 : ");
+		itemmap.putItemList();
 		
-		choose = scan.nextInt();
+		itemmap.delItemList();
 		
-		switch(choose) {
-			case 1:
-				// 입출고 관리
-				break;
-			case 2:
-				// 재고 확인
-				break;
-			case 3:
-				// 매출 확인
-				break;
-		}
+	
 	}
 }
